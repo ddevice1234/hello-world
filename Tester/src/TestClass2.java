944448ddb32c3284212ba300d15f2b091d365aec
Dana <Dana@Dana-PC>
2016-02-09 00:37:39 -0700

    File #2

---
+++ Tester/src/TestClass2.java
@@ -0,0 +1,16 @@
+
+public class TestClass2 {
+
+	/**
+	 * @param args
+	 * 
+	 * Change B
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int big = 35000; 
+		System.out.println((short) big);
+	}
+
+}
