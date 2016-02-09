b8911c7219033e24347f12982c7a0eab8d8975ea
Dana <Dana@Dana-PC>
2016-02-09 00:37:59 -0700

    File #3 commit

---
+++ Tester/src/TestClass3.java
@@ -0,0 +1,16 @@
+
+public class TestClass3 {
+
+	/**
+	 * @param args
+	 * 
+	 * Change B
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int big = 35000; 
+		System.out.println((short) big);
+	}
+
+}
