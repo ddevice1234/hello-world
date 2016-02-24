086da44c0ba6eb9303dbcf48c7377a4af56b9172
DeVice, Dana <Dana.DeVice@anthem.com>
2016-02-23 23:39:13 -0700

    Added a "small" variable and printed it out

--- Tester/src/test/TestClass.java
+++ Tester/src/test/TestClass.java
@@ -6,14 +6,16 @@ public class TestClass {
 	/**
 	 * @param args
 	 * 
-	 * Hello
+	 * Hello I am Sam
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		//this is my algorithm
 		int big = 35000; 
+		int small = 3;
 		System.out.println((short) big);
+		System.out.println(small);
 	}
 
 }
